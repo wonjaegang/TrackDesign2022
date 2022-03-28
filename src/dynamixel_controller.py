8ed948d434e475af21fead5feeb7d7a539771fb0
wonjaegang <robinsam@naver.com>
2022-03-28 14:13:37 +0900

    Revised constants
    Revised constants for dynamixels. Also, removed some useless codes.

--- src/dynamixel_controller.py
+++ src/dynamixel_controller.py
@@ -1,59 +1,59 @@
-import os
+#!/usr/bin/env python
+
 import sys
 import rospy
+import tty
+import termios
 from dynamixel_sdk import *
+from TrackDesign2022.srv import *
+from TrackDesign2022.msg import *
 
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from srv import *
-from msg import *
+fd = sys.stdin.fileno()
+old_settings = termios.tcgetattr(fd)
 
-if os.name == 'nt':
-    import msvcrt
-    def getch():
-        return msvcrt.getch().decode()
-else:
-    import sys, tty, termios
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    def getch():
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+
+def getch():
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
 
 # Control table address
-ADDR_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
-ADDR_GOAL_POSITION      = 116
-ADDR_PRESENT_POSITION   = 132
+ADDR_TORQUE_ENABLE = 24                    # Address of AX-18A. Depends on motor.
+ADDR_GOAL_POSITION = 30
+ADDR_PRESENT_POSITION = 36
 
 # Protocol version
-PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
+PROTOCOL_VERSION = 1.0               # protocol of AX-18A. Depends on motor.
 
 # Default setting
-DXL_ID                      = 1                 # Dynamixel ID : 1
-BAUDRATE                    = 57600             # Dynamixel default baudrate : 57600
-DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
-                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+DXL_ID = 1                 # Dynamixel ID : 1
+BAUDRATE = 1000000           # Dynamixel default baudrate : 57600
+DEVICENAME = '/dev/ttyUSB0'    # Check which port is being used on your controller
 
-TORQUE_ENABLE               = 1                 # Value for enabling the torque
-TORQUE_DISABLE              = 0                 # Value for disabling the torque
-DXL_MINIMUM_POSITION_VALUE  = 0               # Dynamixel will rotate between this value
-DXL_MAXIMUM_POSITION_VALUE  = 1000            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
-DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
+TORQUE_ENABLE = 1                  # Value for enabling the torque
+TORQUE_DISABLE = 0                 # Value for disabling the torque
+DXL_MINIMUM_POSITION_VALUE = 0     # Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE = 1000  # Dynamixel would not move when the position value is out of movable range
+DXL_MOVING_STATUS_THRESHOLD = 20   # Dynamixel moving status threshold
 
 portHandler = PortHandler(DEVICENAME)
 packetHandler = PacketHandler(PROTOCOL_VERSION)
+
 
 def set_goal_pos_callback(data):
     print("Set Goal Position of ID %s = %s" % (data.id, data.position))
     dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, data.id, ADDR_GOAL_POSITION, data.position)
 
+
 def get_present_pos(req):
     dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, req.id, ADDR_PRESENT_POSITION)
     print("Present Position of ID %s = %s" % (req.id, dxl_present_position))
     return dxl_present_position
+
 
 def read_write_py_node():
     rospy.init_node('read_write_py_node')
@@ -61,11 +61,12 @@ def read_write_py_node():
     rospy.Service('get_position', GetPosition, get_present_pos)
     rospy.spin()
 
+
 def main():
     # Open port
     try:
-       portHandler.openPort()
-       print("Succeeded to open the port")
+        portHandler.openPort()
+        print("Succeeded to open the port")
     except:
         print("Failed to open the port")
         print("Press any key to terminate...")
@@ -76,6 +77,7 @@ def main():
     try:
         portHandler.setBaudRate(BAUDRATE)
         print("Succeeded to change the baudrate")
+        print("Current baudrate = %d" % BAUDRATE)
     except:
         print("Failed to change the baudrate")
         print("Press any key to terminate...")
