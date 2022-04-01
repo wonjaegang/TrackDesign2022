386f5ad92feaae14b655083f21cfcc4b6bfc95e2
wonjaegang <robinsam@naver.com>
2022-04-02 02:17:08 +0900

    Add simulation-pose publisher
    - Add pose publisher for simulation. Using pygame, the x-position of mouse pointer in pygame window will be published as a motor position.

---
+++ src/pygame_pose_pub.py
@@ -0,0 +1,29 @@
+import pygame
+from pygame.locals import *
+import rospy
+from geometry_msgs.msg import Point
+
+pygame.init()
+screen = pygame.display.set_mode((1024, 500))
+clock = pygame.time.Clock()
+rate = 50
+
+
+def main():
+    pub = rospy.Publisher('/pygame_pose', Point, queue_size=10)
+    msg = Point
+    msg.y = 0
+    msg.z = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return
+        print(pygame.mouse.get_pos())
+        msg.x = pygame.mouse.get_pos()[0]
+        pub.publish(msg)
+        clock.tick(rate)
+
+
+if __name__ == '__main__':
+    main()
