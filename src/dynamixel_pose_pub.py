61472ce955777ca6efc4539c342bda95e61a9bbd
wonjaegang <robinsam@naver.com>
2022-04-01 12:45:23 +0900

    Revise functions into class methods
    - Revise functions into class methods for better access.
    - Revise velocity extraction code to change goal position when velocity direction is different with goal position.

--- src/dynamixel_pose_pub.py
+++ src/dynamixel_pose_pub.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import numpy as np
 from TrackDesign2022.srv import *
 from TrackDesign2022.msg import *
 import matplotlib.pyplot as plt
@@ -11,7 +12,7 @@ class TrajectoryPub:
         self.node_name = node_name
         self.pub = rospy.Publisher('/set_trajectory', SetTrajectory, queue_size=10)
         self.msg = SetTrajectory()
-        self.rate = 10
+        self.rate = 50
 
     def initialize_node(self):
         rospy.init_node(self.node_name)
@@ -22,8 +23,94 @@ class TrajectoryPub:
         self.msg.velocity = velocity
         self.pub.publish(self.msg)
 
+    def set_position(self, dynamixel_id, goal_position):
+        self.pub_trajectory(dynamixel_id, goal_position, 0)
 
-# ----------------------------------- Functions to plan Trajectory -----------------------------------
+    # ----------------------------------- Methods to extract position & velocity -----------------------------------
+    def linear_position(self, dynamixel_id, current_position, set_position, tg):
+        p1 = current_position
+        p2 = set_position
+        data_array = [p1, p2, tg]
+        for i in range(int(tg * self.rate)):
+            t = (i + 1) / self.rate
+            position = int(trajectory_linear(data_array, t) + 0.5)
+            self.pub_trajectory(dynamixel_id, position, 0)
+            rospy.Rate(self.rate).sleep()
+
+    def linear_velocity(self, dynamixel_id, current_position, set_position, tg):
+        def position2velocity(position):
+            return 50 * self.rate / 114 * position
+        p1 = current_position
+        p2 = set_position
+        data_array = [p1, p2, tg]
+        for i in range(int(tg * self.rate)):
+            if i == 0:
+                t = (i + 1) / self.rate
+                velocity = abs(int(position2velocity(trajectory_linear(data_array, t + 1 / self.rate) -
+                                                     trajectory_linear(data_array, t)) + 0.5))
+            else:
+                t = (i + 1) / self.rate
+                velocity = abs(int(position2velocity(trajectory_linear(data_array, t) -
+                                                     trajectory_linear(data_array, t - 1 / self.rate)) + 0.5))
+            velocity_magnitude = 1 if velocity == 0 else abs(velocity)
+            goal_pos = set_position
+            if (p2 - p1) * velocity < 0:
+                if p2 > p1:
+                    goal_pos = 0
+                else:
+                    goal_pos = 1023
+            self.pub_trajectory(dynamixel_id, goal_pos, velocity_magnitude)
+            rospy.Rate(self.rate).sleep()
+
+    def cubic_position(self, dynamixel_id, current_position, set_position, current_velocity, set_velocity, tg):
+        goal_pos = set_position
+        p1 = current_position
+        p2 = goal_pos
+        v1 = current_velocity
+        v2 = set_velocity
+        data_array = [p1, p2, v1, v2, tg]
+
+        for i in range(int(tg * self.rate)):
+            t = (i + 1) / self.rate
+            position = int(trajectory_cubic(data_array, t) + 0.5)
+            self.pub_trajectory(dynamixel_id, position, 0)
+            rospy.Rate(self.rate).sleep()
+
+    # position: 0 ~ 1023
+    # velocity: -1023 ~ 1023  -> It will be quantized into 0 ~ 1023 in this function.
+    def cubic_velocity(self, dynamixel_id, current_position, set_position, current_velocity, set_velocity, tg):
+        def position2velocity(position):
+            return 50 * self.rate / 114 * position
+        goal_pos = set_position
+        p1 = current_position
+        p2 = goal_pos
+        v1 = current_velocity
+        v2 = set_velocity
+        data_array = [p1, p2, v1, v2, tg]
+
+        # Extract velocity from trajectory spline.
+        for i in range(int(tg * self.rate)):
+            if i == 0:
+                velocity = v1
+            else:
+                t = (i + 1) / self.rate
+                velocity = int(position2velocity(trajectory_cubic(data_array, t) -
+                                                 trajectory_cubic(data_array, t - 1 / self.rate)) + 0.5)
+
+            # Change goal position when velocity direction is different with goal position.
+            velocity_magnitude = 1 if velocity == 0 else abs(velocity)
+            goal_pos = set_position
+            if (p2 - p1) * velocity < 0:
+                if p2 > p1:
+                    goal_pos = 0
+                else:
+                    goal_pos = 1023
+
+            self.pub_trajectory(dynamixel_id, goal_pos, velocity_magnitude)
+            rospy.Rate(self.rate).sleep()
+
+
+# ------------------------------- Functions to create polynomial trajectory ---------------------------------
 def trajectory_linear(data_array, t):
     p1 = data_array[0]
     p2 = data_array[1]
@@ -50,10 +137,6 @@ def trajectory_cubic(data_array, t):
     return trajectory_position
 
 
-def trajectory_quintic(data_array, t):
-    pass
-
-
 def plot_trajectory(data_array, func):
     plot_point = 100
     dt = data_array[-1] / 100
@@ -64,115 +147,40 @@ def plot_trajectory(data_array, func):
     plt.show()
 
 
-# ----------------------------------- Functions to extract position & velocity -----------------------------------
-# Functions that make polynomial spline for the trajectory of Dynamixel.
-def linear_position(set_position, tg):
-    trajectory_pub = TrajectoryPub('dynamixel_trajectory_pub')
-    trajectory_pub.initialize_node()
-
-    goal_pos = set_position
-    p1 = 0
-    p2 = goal_pos
-    data_array = [p1, p2, tg]
-    for i in range(int(tg * trajectory_pub.rate)):
-        t = (i + 1) / trajectory_pub.rate
-        position = int(trajectory_linear(data_array, t) + 0.5)
-        trajectory_pub.pub_trajectory(1, position, 0)
-        rospy.Rate(trajectory_pub.rate).sleep()
-
-
-def linear_velocity(set_position, tg):
-    def position2velocity(position):
-        return 50 * trajectory_pub.rate / 114 * position
-    trajectory_pub = TrajectoryPub('dynamixel_trajectory_pub')
-    trajectory_pub.initialize_node()
-
-    goal_pos = set_position
-    p1 = 0
-    p2 = goal_pos
-    data_array = [p1, p2, tg]
-    for i in range(int(tg * trajectory_pub.rate)):
-        if i == 0:
-            t = (i + 1) / trajectory_pub.rate
-            velocity = abs(int(position2velocity(trajectory_linear(data_array, t + 1 / trajectory_pub.rate) -
-                                                 trajectory_linear(data_array, t)) + 0.5))
-        else:
-            t = (i + 1) / trajectory_pub.rate
-            velocity = abs(int(position2velocity(trajectory_linear(data_array, t) -
-                                                 trajectory_linear(data_array, t - 1 / trajectory_pub.rate)) + 0.5))
-        if velocity == 0:
-            velocity = 1
-        trajectory_pub.pub_trajectory(1, goal_pos, velocity)
-        rospy.Rate(trajectory_pub.rate).sleep()
-
-
-def cubic_position(set_position, tg):
-    trajectory_pub = TrajectoryPub('dynamixel_trajectory_pub')
-    trajectory_pub.initialize_node()
-
-    goal_pos = set_position
-    p1 = 0
-    p2 = goal_pos
-    v1 = 0
-    v2 = 0
-    data_array = [p1, p2, v1, v2, tg]
-
-    for i in range(int(tg * trajectory_pub.rate)):
-        t = (i + 1) / trajectory_pub.rate
-        position = int(trajectory_cubic(data_array, t) + 0.5)
-        trajectory_pub.pub_trajectory(1, position, 0)
-        rospy.Rate(trajectory_pub.rate).sleep()
-
-
-def cubic_velocity(set_position, tg):
-    def position2velocity(position):
-        return 50 * trajectory_pub.rate / 114 * position
-    trajectory_pub = TrajectoryPub('dynamixel_trajectory_pub')
-    trajectory_pub.initialize_node()
-
-    goal_pos = set_position
-    p1 = 0
-    p2 = goal_pos
-    v1 = 0
-    v2 = 0
-    data_array = [p1, p2, v1, v2, tg]
-    for i in range(int(tg * trajectory_pub.rate)):
-        if i == 0:
-            velocity = v1
-        else:
-            t = (i + 1) / trajectory_pub.rate
-            velocity = abs(int(position2velocity(trajectory_cubic(data_array, t) -
-                                                 trajectory_cubic(data_array, t - 1 / trajectory_pub.rate)) + 0.5))
-        if velocity == 0:
-            velocity = 1
-        trajectory_pub.pub_trajectory(1, goal_pos, velocity)
-        rospy.Rate(trajectory_pub.rate).sleep()
-
-
+# ----------------------------------- Main function -----------------------------------
 def main():
-    cubic_position(0, 0.5)
-    rospy.Rate(0.5).sleep()
+    trajectory_pub = TrajectoryPub('dynamixel_trajectory_pub')
+    trajectory_pub.initialize_node()
 
-    linear_position(1023, 1)
-    rospy.Rate(0.5).sleep()
-
-    cubic_position(0, 0.5)
-    rospy.Rate(0.5).sleep()
-
-    linear_velocity(1023, 1)
-    rospy.Rate(0.5).sleep()
-
-    cubic_position(0, 0.5)
-    rospy.Rate(0.5).sleep()
-
-    cubic_position(1023, 1)
-    rospy.Rate(0.5).sleep()
-
-    cubic_position(0, 0.5)
-    rospy.Rate(0.5).sleep()
-
-    cubic_velocity(1023, 1)
-    rospy.Rate(0.5).sleep()
+    dynamixel_id = 1
+    start_pos = 0
+    end_pos = 1023
+    start_vel = 0
+    end_vel = 0
+    time_goal = 1
+    while True:
+        input_str = input()
+        if input_str[:4] == 'rate':
+            trajectory_pub.rate = int(input_str[4:])
+            print("Rate changed to %dHz" % trajectory_pub.rate)
+        elif input_str == 'lp':
+            print("linear trajectory, position control")
+            trajectory_pub.linear_position(dynamixel_id, start_pos, end_pos, time_goal)
+        elif input_str == 'lv':
+            print("linear trajectory, velocity control")
+            trajectory_pub.linear_velocity(dynamixel_id, start_pos, end_pos, time_goal)
+        elif input_str == 'cp':
+            print("cubic trajectory, position control")
+            trajectory_pub.cubic_position(dynamixel_id, start_pos, end_pos, start_vel, end_vel, time_goal)
+        elif input_str == 'cv':
+            print("cubic trajectory, velocity control")
+            trajectory_pub.cubic_velocity(dynamixel_id, start_pos, end_pos, start_vel, end_vel, time_goal)
+        elif input_str == 'i':
+            print("Initialize position")
+            trajectory_pub.set_position(dynamixel_id, start_pos)
+        else:
+            break
+        rospy.Rate(0.5).sleep()
 
 
 if __name__ == '__main__':
