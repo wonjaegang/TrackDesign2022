e7b5b2b1c41767cc2ab19c513bd206ae7a7fe9d7
Taek-16 <ffjjgghh16@naver.com>
2022-05-13 15:25:30 +0900

    invere_done??

--- src/inverse_kinematics.py
+++ src/inverse_kinematics.py
@@ -7,30 +7,38 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 # IKpy imports
-from ikpy import chain
+from ikpy import chain, inverse_kinematics
+from ikpy.utils import plot
 
 #msg
 from std_msgs.msg import Float32MultiArray
 
-
-
-def ik(target):
+def ik(target, initial):
 
     inverse = chain.Chain.from_urdf_file("src/TrackDesign2022/urdf/meta_arm.xacro")
+    print(inverse)
+    inverse.active_links_mask[0] = False
+    inverse.active_links_mask[7] = False
 
     # 4X4 Transformation matrix
-    quat = R.from_quat(target[0:4])
-    rotation = quat.as_matrix()
-    matrix_a = np.vstack((rotation,[0,0,0]))
-    matrix_b = np.array([np.hstack((target[4:7],1))])
-    transformation = np.hstack((matrix_a,matrix_b.T))
+    def trans(goal_data):
+        quat = R.from_quat(goal_data[0:4])
+        rotation = quat.as_matrix()
+        matrix_a = np.vstack((rotation,[0,0,0]))
+        matrix_b = np.array([np.hstack((goal_data[4:7],1))])
+        transformation = np.hstack((matrix_a,matrix_b.T))
+        return transformation
 
     # Calculate inverse kinematics
-    ik_angle = inverse.inverse_kinematics_frame(transformation)
-    goal_position = ik_angle[1:7]
+    ik_angle = inverse.inverse_kinematics_frame(trans(target),initial_position = initial)
+    goal_position = ik_angle
+    print("The angles of each joints are : ", goal_position[1:7])
 
-    print("The angles of each joints are : ", goal_position)
-
+    '''
+    fig, ax = plot.init_3d_figure()
+    inverse.plot(ik_angle, ax, target[4:7])
+    plot.show_figure()
+    '''
     return goal_position
 
 
@@ -40,17 +48,19 @@ def main():
     goal_position_pub = rospy.Publisher('/goal_position', Float32MultiArray,queue_size=10)
     goal = Float32MultiArray()
     oculus = oculus_sub()
+    initial = [0, 0, 0, 0.0001, -0.0001, 0.0001, 0.0001, 0]
     rospy.sleep(0.5)
     print("start!!!!!")
 
     while not rospy.is_shutdown():
 
-        #goal.data = s.ik([-0.363, 0.609, 0.383, 0.592, 0.3, -0.1, -0.6])
-        print("current pose", oculus.right_pose)
-        goal.data = ik(oculus.right_pose)
+        #ik_goal = ik([0, -0.451, 0.544, 0.707, 0.0, -0.5, -0.2], initial)
+        #print("current pose", oculus.right_pose)
+        ik_goal = ik(oculus.right_pose, initial)
+        goal.data = ik_goal[1:7]
+        initial = ik_goal
 
         goal_position_pub.publish(goal)
-        #rospy.sleep(0.1)
 
 if __name__ == '__main__':
     main()
--- src/inverse_test.py
+++ src/inverse_test.py
@@ -57,22 +57,23 @@ def main():
 
 
     inverse = chain.Chain.from_urdf_file("src/TrackDesign2022/urdf/meta_arm.xacro")
-
+'''
     inverse.active_links_mask[0] = False
     inverse.active_links_mask[7] = False
-
-    print('!!!!!!!1', inverse)
+'''
+    print('!!!!!!!', inverse)
 
     # 4X4 Transformation matrix
-    rq = R.from_quat(target[0:4])
-    rm = rq.as_matrix()
-    ma = np.vstack((rm,[0,0,0]))
-    mb = np.array([np.hstack((target[4:7],1))])
-    tm = np.hstack((ma,mb.T))
+    quat = R.from_quat(target[0:4])
+    rotation = quat.as_matrix()
+    matrix_a = np.vstack((rotation,[0,0,0]))
+    matrix_b = np.array([np.hstack((target[4:7],1))])
+    transformation = np.hstack((matrix_a,matrix_b.T))
 
     # Calculate inverse kinematics
-    ik = inverse.inverse_kinematics_frame(tm)
-    goal_position = ik[1:7]
+    ik_angle = inverse.inverse_kinematics_frame(transformation)
+    goal_position = ik_angle[1:7]
+
     print("The angles of each joints are : ", goal_position)
 
     goal.data = goal_position
