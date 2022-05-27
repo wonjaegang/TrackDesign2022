96d746b11912bfff5cfdbfa20b19360b6dacbef5
wonjaegang <robinsam@naver.com>
2022-05-27 16:44:21 +0900

    Add code for deepcopy

--- src/pygame_pose_pub.py
+++ src/pygame_pose_pub.py
@@ -4,20 +4,21 @@ import pygame
 from pygame.locals import *
 import rospy
 from TrackDesign2022.msg import *
+import copy
 
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 clock = pygame.time.Clock()
-rate = 20
+rate = 10
 
 
 def main():
-    pub = rospy.Publisher('/set_trajectory', SetTrajectoryArray, queue_size=10)
+    pub = rospy.Publisher('/set_trajectory', SetTrajectoryArray, queue_size=1)
     rospy.init_node('pygame_pose_pub')
     msg = SetTrajectory()
 
     # Set the motors which user wants to control
-    control_motors = [6, 16]
+    control_motors = [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16]
 
     while True:
         for event in pygame.event.get():
@@ -31,76 +32,77 @@ def main():
         msg.position = -(pygame.mouse.get_pos()[0] - 250) / 250 * 90
         msg.velocity = 60
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 2
         msg.position = -(pygame.mouse.get_pos()[0]) / 500 * 90
         msg.velocity = 60
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 3
         msg.position = (pygame.mouse.get_pos()[0] - 250) / 250 * 150
         msg.velocity = 400
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 4
         msg.position = (pygame.mouse.get_pos()[0] - 500) / 500 * 90
         msg.velocity = 200
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 5
         msg.position = (pygame.mouse.get_pos()[0] - 250) / 250 * 150
         msg.velocity = 400
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 6
         msg.position = -(pygame.mouse.get_pos()[0] - 250) / 250 * 90
         msg.velocity = 400
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 11
         msg.position = (pygame.mouse.get_pos()[0] - 250) / 250 * 90
         msg.velocity = 60
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 12
         msg.position = (pygame.mouse.get_pos()[0]) / 500 * 90
         msg.velocity = 60
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 13
         msg.position = (pygame.mouse.get_pos()[0] - 250) / 250 * 150
         msg.velocity = 400
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 14
         msg.position = -(pygame.mouse.get_pos()[0] - 500) / 500 * 90
         msg.velocity = 200
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 15
         msg.position = (pygame.mouse.get_pos()[0] - 250) / 250 * 150
         msg.velocity = 400
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg.id = 16
         msg.position = (pygame.mouse.get_pos()[0] - 250) / 250 * 90
         msg.velocity = 400
         if msg.id in control_motors:
-            data_array.append(msg)
+            data_array.append(copy.deepcopy(msg))
 
         msg_array = SetTrajectoryArray()
         msg_array.data = data_array
+        print(msg_array)
         pub.publish(msg_array)
 
         clock.tick(rate)
